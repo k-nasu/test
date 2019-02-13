c0823e32f871543212f4b6622f6411a6e9404b0c
k-nasu <nskn18dneilch@gmail.com>
2019-02-13 19:46:06 +0900

    20190212

--- TestDB/src/CheckLoadJDBC.java
+++ TestDB/src/CheckLoadJDBC.java
@@ -1,16 +1,16 @@
 
 public class CheckLoadJDBC {
 
-	public static void main(String[] args)throws
-	InstantiationException,IllegalAccessException{
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException{
+		// TODO 自動生成されたメソッド・スタブ
 		String msg ="";
+
 		try{
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
-		msg ="ドライバのロードに失敗しました";
-		}catch(ClassNotFoundException e){
-		msg ="ドライバのロードに失敗しました";
+		msg ="ドライバのロードに成功しました";
+		} catch(ClassNotFoundException e){
+			msg ="ドライバのロードに失敗しました";
 		}
 		System.out.println(msg);
 	}
-
 }
