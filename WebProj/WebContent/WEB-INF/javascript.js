b2a63316fdd4ea644ff0ea11a047bd04bb49d9d9
k-nasu <nskn18dneilch@gmail.com>
2019-03-13 19:14:14 +0900

    20190313

---
+++ WebProj/WebContent/WEB-INF/javascript.js
@@ -0,0 +1,14 @@
+<DOCTYPE! html>
+<html lang="ja">
+    
+<head>
+<meta charset="htf-8">
+<title>fsa</title>
+</head>
+
+<body>
+for (var a=1; a<5; a++){
+    document.write("★");
+}
+</body>
+</html>
